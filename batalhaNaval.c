6ce4f91e48be5a55a907d6e1cba11854ce5f116c
dreadnokkk <pedroaiello.delazari1@gmail.com>
2025-04-26 09:43:09 +0000

    finished work

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,107 @@
 #include <stdio.h>
+#define linhas 10
+#define colunas 10
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+    int main(){
+                
+        char letras[] = {'A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K'};
+        int matriz [linhas] [colunas] = {};
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+        //loops aninhados para posicionar os 3 barcos
+        
+        //barcos na diagonal        
+        for (int j = 0 ; j <= 2; j++){  
+            matriz[j][j] = 3;  
+        }
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+        for(int i = 5; i <= 7; i++){
+            for(int j = 7 - i; j <= 7 - i; j++){
+                matriz[i][j] = 3;
+            }
+        }
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+        //barco na horizontal
+        for(int i = 5; i <= 5; i++){ // O sinal de "=" faz toda diferença ou igualar ele a um número a mais por exemplo 5(daria o mesmo resultado), pois eu posso escolher qualquer linha e igualar para o loop não geral nenhuma outra linha 
+          for(int j = 7; j <= 9; j++){// Esse loop faz com que o númeo 3 gere deitado na linha 4 por conta do ultimo código que eu fiz igualando o número a 4
+            matriz[i][j] = 3;
+          }
+        }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
 
-    return 0;
+        //barco na vertical
+        for(int i = 1; i <= 3; i++){// Mesmo racíoninio do último for PORÉM o 3 vai ser adicionado verticalmente por conta que o "I" imprime só linhas em pé e não colunas deitadas igual o "J"
+            for(int j = 9; j <= 9; j++){//Selecionei a coluna 2 "C" e igualei no loop para n gerar mais nehuma outra fileira e deixar só uma.
+               matriz[i][j] = 3;
+            }
+        }
+
+        //Bloco de código que irá imprimir o cone
+        for(int i = 0; i <= 2; i++){
+            for(int j = 2 - i; j <= 2 + i; j++){
+                if(matriz[i][j] == 3){
+                   matriz[i][j] = 5;
+                }else{
+                matriz[i][j] = 1;
+            }
+        }
+    }
+
+        //Bloco de código que irá imprimir a cruz
+        for(int i = 3; i <= 7; i++){
+            for(int j = 4; j <= 4; j++){
+                if(i == 5){ // Igualei o I a 5, pois quando chegar na linh 5 o IF irá adicionar mais casas a essa coluna
+                    for(int j = 2; j <= 6; j++){
+                    if(matriz[i][j] == 3){ //Linha de correção caso há contado com algum navio nas linhas
+                        matriz[i][j] = 5;
+                    }else{
+                        matriz[i][j] = 2;
+                    }
+               }
+            }
+            if(matriz[i][j] == 3){ //Linha de correção caso há contado com algum navio nas colunas
+                matriz[i][j] = 5;
+            }else{
+                matriz[i][j] = 2;
+            }
+        }
+    }
+      //Bloco de código que vai imprimir o octógono 
+      
+      for (int i = 0; i <= 2; i++) {
+        for (int j = 7 - i; j <= 7 + i; j++) {
+            if (i == 2) {
+                // Loop interno com variáveis diferentes para evitar conflito
+                for (int k = 4; k >= 3; k--) { 
+                    for (int l = 3 + k; l <= 11 - k; l++) { 
+                        if (matriz[k][l] == 3) {
+                            matriz[k][l] = 5;
+                        } else {
+                            matriz[k][l] = 4;
+                        }
+                    }
+                }
+            }
+            // Verifica e altera o valor na matriz
+            if (matriz[i][j] == 3) {
+                matriz[i][j] = 5;
+            } else {
+                matriz[i][j] = 4;
+            }
+        }
+    }
+
+        printf(" TABULEIRO BATALHA NAVAL\n");
+        printf("   ");
+        for(int i = 0; i <= 9; i++){
+        printf("%c ", letras[i]); // Imprime letras das colunas
+    }
+        printf("\n");
+        for(int i = 0; i <= 9; i++){
+        printf("%2d ", i + 1);  //Imprime as linhas de 1 até 10 
+        for(int j = 0; j <= 9; j++){ // Imprime o número 0 da tabela representando a água
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 }
